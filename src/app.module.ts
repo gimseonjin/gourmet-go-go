b5f7df69e231a7c272370703cb0fd7024c919087
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2024-02-20 11:49:23 +0900

    chore: remove app controller and service, add business, presentation, persistence, and database modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BusinessModule } from './business/business.module';
+import { PresentationModule } from './presentation/presentation.module';
+import { PersistenceModule } from './persistence/persistence.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BusinessModule, PresentationModule, PersistenceModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
